47a3b2596c083c89e674b94a6369f3b4f774abb0
Zaid-Ar <zaidhamid600@gmail.com>
2024-07-26 18:02:37 +0400

    first commit

---
+++ alx_book_store.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Books(
+    book_id INT PRIMARY_KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors(
+    author_id INT PRIMARY_KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+    customer_id(PRIMARY_KEY),
+    customer_name VARCHAR(215),
+    email VARCHAR(215)
+);
+
+CREATE TABLE Orders(
+    order_id INT PRIMARY_KEY,
+    customer_id INT
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+)
+
+CREATE TABLE Orders_Details(
+    order_details_id INT PRIMARY_KEY,
+    order_id INT, 
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    book_id INT,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    quantity DOUBLE
+) 
