fa618b2216573eeb3a7873dcfe6c98715fe51818
Zaid-Ar <zaidhamid600@gmail.com>
2024-07-26 18:25:02 +0400

    fix 7

---
+++ MySQLServer.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+# Database connection details (replace with your own)
+try:
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="mydb"
+)
+finally: 
+    print("can't connect ")
+mycursor = mydb.cursor()
+
+# Create a table named `customers` (if it doesn't exist)
+mycursor.execute("""
+CREATE DATABASE IF NOT EXISTS alx_book_store
+""")
+
+print("Database 'alx_book_store' created successfully!")
+
+# Close connections
+mycursor.close()
+mydb.close()
+
+print("Database connection closed.")
