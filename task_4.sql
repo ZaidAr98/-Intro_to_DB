afd0d683421ce226dca28a2627e4d5308deffb4f
Zaid-Ar <zaidhamid600@gmail.com>
2024-07-26 18:47:23 +0400

    fix 15

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,12 @@
-SHOW TABLES; 
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA 
+FROM 
+    information_schema.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
