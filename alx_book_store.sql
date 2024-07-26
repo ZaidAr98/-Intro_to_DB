b45603ad2af2254995c4e505822ffaab73c32ce0
Zaid-Ar <zaidhamid600@gmail.com>
2024-07-26 18:10:40 +0400

    fix 3

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store
 USE alx_book_store
 
 CREATE TABLE Books(
-    book_id INT PRIMARY_KEY,
+    book_id  INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id),
@@ -11,24 +11,24 @@ CREATE TABLE Books(
 );
 
 CREATE TABLE Authors(
-    author_id INT PRIMARY_KEY,
+    author_id  INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers(
-    customer_id(PRIMARY_KEY),
+    customer_id  INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215),
     email VARCHAR(215)
 );
 
 CREATE TABLE Orders(
-    order_id INT PRIMARY_KEY,
+    order_id  INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 )
 
 CREATE TABLE Orders_Details(
-    order_details_id INT PRIMARY_KEY,
+    order_details_id  INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT, 
     FOREIGN KEY (order_id) REFERENCES Orders(order_id)
     book_id INT,
